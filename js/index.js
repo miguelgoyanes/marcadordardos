b331490deac60ebce581fcf57a2be9d395eaafed
Your Name <miangelgoyanes@gmail.com>
2023-08-20 09:50:21 +0200

    barra de navegacion sin responsive

--- js/index.js
+++ js/index.js
@@ -9,6 +9,16 @@ function Jugar() {
         // Redireccionar a otra página dentro del mismo sitio
         window.location.href = "/seleccionJuego.html";
     })
+    let jugar501 = document.getElementById("btn-jugar-501")
+    jugar501.addEventListener("click", e => {
+        // Redireccionar a otra página dentro del mismo sitio
+        window.location.href = "/seleccionJuego.html";
+    })
+    let jugar701 = document.getElementById("btn-jugar-701")
+    jugar701.addEventListener("click", e => {
+        // Redireccionar a otra página dentro del mismo sitio
+        window.location.href = "/seleccionJuego.html";
+    })
 }
 function saberMas() {
     let saberCricket = document.getElementById("btn-saber-cricket")
@@ -18,6 +28,14 @@ function saberMas() {
     let saber301 = document.getElementById("btn-saber-301")
     saber301.addEventListener("click", e => {
         window.location.href = "/comoJugar301.html";
+    })
+    let saber501 = document.getElementById("btn-saber-501")
+    saber501.addEventListener("click", e => {
+        window.location.href = "/comoJugar501.html";
+    })
+    let saber701 = document.getElementById("btn-saber-701")
+    saber701.addEventListener("click", e => {
+        window.location.href = "/comoJugar701.html";
     })
 }
 
