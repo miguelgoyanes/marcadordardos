d2ff6fe64b914806ad1354bdf0b74c8e84729a98
Your Name <miangelgoyanes@gmail.com>
2023-09-08 10:44:19 +0200

    carga del navegador desde js

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,62 @@
-const menu = document.querySelector('.hamburguesa');  
-const navegacion = document.querySelector('.contenedor-navegacion');
-
 document.addEventListener('DOMContentLoaded',()=>{
-    menu.addEventListener('click', abrirMenu);
-    
+    importarNavegador()
+    importarFooter()
 })
+const importarNavegador = () => {
+    const navegador = document.getElementById("navegador")
+    let contenidoNavegador = 
+        `<div class="logo"><a href="index.html"><img src="./img/dardo.png" alt="logo marcador dardos"></a></div>
+        <div class="contenedor-navegacion ocultar-nav">
+            <ul class="menu-horizontal">
+                <li><a class="element-menu" href="./index.html">Home</a></li>
+                <li><a class="element-menu" href="./seleccionJuego.html">Jugar</a></li>
+                <li>
+                    <a class="element-menu-desplegable" href="#">Como jugar <i
+                            class="fa-solid fa-angle-down fa-xs"></i></a>
+                    <ul class="menu-vertical">
+                        <li><a class="element-menu" href="comoJugarCricket.html">Cricket</a></li>
+                        <li><a class="element-menu" href="comoJugar301.html">301</a></li>
+                        <li><a class="element-menu" href="comoJugar501.html">501</a></li>
+                        <li><a class="element-menu" href="comoJugar701.html">701</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="element-menu-desplegable" href="#">Ayuda<i
+                            class="fa-solid fa-angle-down fa-xs"></i></a>
+                    <ul class="menu-vertical">
+                        <li><a class="element-menu" href="comoJugarCricket.html">Equipamiento</a></li>
+                        <li><a class="element-menu" href="comoJugar301.html">Técnicas y Consejos</a></li>
+                        <li><a class="element-menu" href="comoJugar501.html">Entrenamiento</a></li>
+                        <li><a class="element-menu" href="comoJugar701.html">Curiosidades</a></li>
+                        <li><a class="element-menu" href="comoJugar701.html">Preguntas Frecuentes</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <div class="hamburguesa">
+            <span></span><span></span><span></span>
+        </div>`
+    navegador.innerHTML = contenidoNavegador
+
+    // hacemos abrible el menu
+    const menu = document.querySelector('.hamburguesa');  
+    menu.addEventListener('click', abrirMenu);
+}   
+
+const importarFooter = () => {
+    const footer = document.getElementById("footer")
+    let contenidoFooter = 
+        `<div>
+            <a href="sobreNosotros.html">Contactanos</a>
+        </div>
+        <div>
+            <a href="politicaPrivacidad.html">Politica de privacidad</a>
+        </div>`
+        footer.innerHTML = contenidoFooter
+} 
 
 const abrirMenu = () => {
+    const navegacion = document.querySelector('.contenedor-navegacion');
     navegacion.classList.remove('ocultar-nav');
     CrearBotonCerrar();
     crearElementosDesplegables()
@@ -18,6 +68,7 @@ const CrearBotonCerrar = () => {
 
     btnCerrar.textContent = 'x'; //añadimos contenido
     btnCerrar.classList.add('btn-cerrar');
+    const navegacion = document.querySelector('.contenedor-navegacion');
     navegacion.appendChild(btnCerrar);
     cerrarMenu(btnCerrar);
 }
@@ -57,6 +108,7 @@ const eliminamosElementosDesplegables = () =>{
 
 const cerrarMenu = (boton) => {
     boton.addEventListener('click',()=>{
+        const navegacion = document.querySelector('.contenedor-navegacion');
         navegacion.classList.add('ocultar-nav');
         boton.remove();
         eliminamosElementosDesplegables()
@@ -71,12 +123,3 @@ const cerrarMenu = (boton) => {
     })
 }
 
-
-// function ponerPuntosElementosMenu() {
-//     if (window.innerWidth <= "768") {
-//         const elementosMenu = document.querySelectorAll(".menu-vertical > li > a ")
-//         elementosMenu.forEach(elemento => {
-//             elemento.innerText = "- " + elemento.innerText
-//         })
-//     }
-// }
